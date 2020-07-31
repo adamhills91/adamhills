a152145aef25bc6c928a53db29212a9c1256e998
Adam Hills <adamhills91@gmail.com>
2020-07-31 22:48:42 +0100

    cleaned up javascript code, added links to linkedin and github

--- script.js
+++ script.js
@@ -1,82 +1,124 @@
-const nav = document.getElementById("nav-bar");
-const header = document.querySelector("header");
-const menuCheckBox = document.getElementById("menu");
-const body = document.querySelector("body");
-const links = document.querySelectorAll(".nav-link");
-let date = new Date();
-let hours = date.getHours();
-const greeting = document.getElementById("greeting");
-const mainTitle = document.getElementById("main-title");
-const hireButton = document.getElementById("hire-button");
-const profilePhoto = document.getElementById("profile-photo");
-const aboutSection = document.getElementById("about-content");
-const servicesSection = document.getElementById("services");
-const serviceCards = document.querySelectorAll(".services-card");
-const projectTitle = document.getElementById("project-title");
-const projectTiles = document.querySelectorAll(".project-tile");
-const projectText = document.querySelectorAll(".project-text");
-const contactSection = document.getElementById("contact");
-let prevScroll;
-const headerHeight = 80;
-const mqMobile = window.matchMedia("(max-width 1024px");
+//Set the greeting message depending on the current time
+let date = new Date(),
+  hours = date.getHours();
+const greeting = document.getElementById("greeting"),
+  setGreeting = (hours) => {
+    if (hours > 4 && hours < 12) {
+      greeting.textContent = "Good morning!";
+    } else if (hours > 11 && hours < 17) {
+      greeting.textContent = "Good afternoon!";
+    } else if (hours > 16 && hours < 22) {
+      greeting.textContent = "Good evening!";
+    }
+  };
+setGreeting(hours);
 
-if (hours > 4 && hours < 12) {
-  greeting.textContent = "Good morning!";
-} else if (hours > 11 && hours < 17) {
-  greeting.textContent = "Good afternoon!";
-} else if (hours > 16 && hours < 22) {
-  greeting.textContent = "Good evening!";
-}
-
-if (window.screen.width > 767) {
-  setTimeout(function () {
-    aboutSection.style.opacity = 1;
-  }, 1700);
-}
-
-const toggleNavDisplay = () => {
-  if (menuCheckBox.checked == false) {
-    nav.style.width = "0";
-    body.style.overflow = "";
-    console.log("hello");
-  } else {
-    nav.style.width = "100%";
-    body.style.overflow = "hidden";
-    console.log("hello");
-  }
-};
+// Open and close the nav menu when the hamburger icon is clicked
+const nav = document.getElementById("nav-bar"),
+  body = document.querySelector("body"),
+  menuCheckBox = document.getElementById("menu"),
+  toggleNavDisplay = () => {
+    if (menuCheckBox.checked == false) {
+      closeNav();
+    } else {
+      openNav();
+    }
+  };
 menuCheckBox.addEventListener("click", toggleNavDisplay);
 
-const closeNav = () => {
-  if (window.innerWidth < 1000) {
-    nav.style.width = "0";
-    body.style.overflow = "";
-    menuCheckBox.checked = false;
-  }
+// Function that opens the nav menu
+const openNav = () => {
+  nav.style.width = "100%";
+  body.style.overflow = "hidden";
 };
-links.forEach((e) => e.addEventListener("click", closeNav));
+// Function that closes the nav menu
+const closeNav = () => {
+  nav.style.width = "0";
+  body.style.overflow = "";
+  menuCheckBox.checked = false;
+};
 
+// Closes the nav menu and scrolls to section when one of the nav links is clicked
+const links = document.querySelectorAll(".nav-link");
+links.forEach((e) =>
+  e.addEventListener("click", () => {
+    let destination = `${e.id.replace("-link", "")}Section`;
+    eval(destination).scrollIntoView({ behavior: "smooth" });
+    if (window.innerWidth < 1000) {
+      closeNav();
+      if (destination != "homeSection") {
+        header.classList.add("header-up");
+        console.log("hello");
+      }
+    }
+  })
+);
+
+// Stops the nav bar remaining open when the window size is decreased, and prevents the profile photo from transitioning around.
+const profilePhoto = document.getElementById("profile-photo");
+let screenToggle;
+if (window.innerWidth >= 1000) {
+  screenToggle = false;
+} else {
+  screenToggle = true;
+}
 window.addEventListener("resize", () => {
-  if (window.innerWidth >= 1000) {
+  if (window.innerWidth >= 1000 && !screenToggle) {
+    screenToggle = true;
     nav.style.width = "";
     nav.style.transition = "none";
     profilePhoto.style.transition = "none";
     body.style.overflow = "";
-  } else {
+  } else if (window.innerWidth < 1000 && screenToggle) {
+    screenToggle = false;
     nav.style.transition = "all 0.3s ease-out";
     closeNav();
   }
 });
 
+// Causes different sections to fade in when the user has scrolled a certain distance down the page
+let prevScroll;
+const header = document.querySelector("header"),
+  headerHeight = 80,
+  homeSection = document.getElementById("intro"),
+  aboutSection = document.getElementById("about-content"),
+  servicesSection = document.getElementById("services"),
+  serviceCards = document.querySelectorAll(".services-card"),
+  projectsSection = document.getElementById("projects"),
+  projectTitle = document.getElementById("project-title"),
+  projectTiles = document.querySelectorAll(".project-tile"),
+  contactSection = document.getElementById("contact"),
+  sections = [contactSection, projectsSection, servicesSection, aboutSection];
+
+for (let i = 0; i < sections.length; i++) {}
 window.addEventListener("scroll", () => {
   let scroll = this.scrollY;
-  if (window.screen.width < 767) {
-    setFadeInPosition(scroll, 450, 1050, 1700, 3700);
-  } else if (window.screen.width > 767) {
-    setFadeInPosition(scroll, 0, 500, 1000, 2100);
-    aboutSection.style.opacity = 1;
-  }
+  let time = 300;
+  sections.forEach((item) => {
+    if (scroll > item.offsetTop - 400) {
+      if (item === servicesSection) {
+        //   // Causes services cards to fade in one after the other by *time* milliseconds.
+        serviceCards.forEach((e) => {
+          setTimeout(function () {
+            e.style.opacity = 1;
+          }, time);
+          time += 300;
+        });
+      } else if (item === projectsSection) {
+        projectTitle.style.opacity = 1;
+        // Causes project tiles to fade in one after the other by *time* milliseconds.
+        projectTiles.forEach((e) => {
+          setTimeout(function () {
+            e.style.opacity = 1;
+          }, time);
+          time += 300;
+        });
+      }
+      item.style.opacity = 1;
+    }
+  });
 
+  // Displays the header when the user scrolls up
   if (scroll > prevScroll) {
     if (scroll > headerHeight) {
       header.classList.add("header-up");
@@ -87,45 +129,17 @@ window.addEventListener("scroll", () => {
   prevScroll = scroll;
 });
 
-const setFadeInPosition = (
-  scroll,
-  aboutPos,
-  servicesPos,
-  projectsPos,
-  contactPos
-) => {
-  if (scroll > aboutPos) {
+// If large screen size, fade in about section on page load to avoid visible blank section
+if (window.screen.width > 767) {
+  setTimeout(function () {
     aboutSection.style.opacity = 1;
-  }
-  if (scroll > servicesPos) {
-    servicesSection.style.opacity = 1;
-    let time = 300;
-    for (let i = 0; i < serviceCards.length; i++) {
-      setTimeout(function () {
-        serviceCards[i].style.opacity = 1;
-      }, time);
-      time += 300;
-    }
-  }
-  if (scroll > projectsPos) {
-    projectTitle.style.opacity = 1;
-    time = 300;
-    for (let i = 0; i < projectTiles.length; i++) {
-      setTimeout(function () {
-        projectTiles[i].style.opacity = 1;
-      }, time);
-      time += 300;
-    }
-  }
-  if (scroll > contactPos) {
-    contactSection.style.opacity = 1;
-  }
-};
-
+  }, 1700);
+}
 setTimeout(function () {
   greeting.classList.toggle("inactive");
 }, 500);
 
+const mainTitle = document.getElementById("main-title");
 setTimeout(function () {
   mainTitle.classList.toggle("inactive");
 }, 800);
@@ -133,20 +147,32 @@ setTimeout(function () {
 setTimeout(function () {
   profilePhoto.classList.toggle("inactive");
 }, 1500);
-
+const hireButton = document.getElementById("hire-button");
 setTimeout(function () {
   hireButton.classList.toggle("inactive");
 }, 1100);
 
+hireButton.addEventListener("click", () => {
+  contactSection.scrollIntoView({ behavior: "smooth" });
+});
+
+const projectsButton = document.getElementById("projects-button");
+projectsButton.addEventListener("click", () => {
+  projectsSection.scrollIntoView({ behavior: "smooth" });
+});
+// Displays information over project tiles when they are clicked
+const projectText = document.querySelectorAll(".project-text");
 projectTiles.forEach((item) => {
-  item.addEventListener("click", () => {
-    if (item.childNodes[3].style.opacity == 0) {
-      for (let i = 0; i < projectTiles.length; i++) {
-        projectText[i].style.opacity = 0;
+  if (window.innerWidth < 1000) {
+    item.addEventListener("click", () => {
+      if (item.childNodes[3].style.opacity == 0) {
+        for (let i = 0; i < projectTiles.length; i++) {
+          projectText[i].style.opacity = 0;
+        }
+        item.childNodes[3].style.opacity = 1;
+      } else {
+        item.childNodes[3].style.opacity = 0;
       }
-      item.childNodes[3].style.opacity = 1;
-    } else {
-      item.childNodes[3].style.opacity = 0;
-    }
-  });
+    });
+  }
 });
